2c4518a0d3ab82119b0dcf0912d0eea7a6ef3511
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-13 18:58:36 +0530

    [Daxil] Added:Application properties for dev and production

--- src/main/java/com/bridgelabz/employeepayrollserviceapp/EmployeePayrollServiceApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollserviceapp/EmployeePayrollServiceApplication.java
@@ -3,14 +3,16 @@ package com.bridgelabz.employeepayrollserviceapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollServiceApplication.class, args);
-        log.info("Employee Payroll App Started SuccessFully");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollServiceApplication.class, args);
+        log.info("Employee Payroll Application Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
 
 }
